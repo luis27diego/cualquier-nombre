187fbd6a11897b93209927f1d96c2f6e5af30cdc
catalina jara braem <catalinajarabraem@hotmail.com>
2025-03-12 12:05:22 -0300

    agrego funciones

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,8 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+
+def funcion ():
+    print ("taka")
+
+def pititrax():
+    print("tuki")
